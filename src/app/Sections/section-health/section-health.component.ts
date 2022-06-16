a43735b1a0c22cc943f56457a71c0d76c19d607d
LokTar76 <omg23333@gmail.com>
2022-06-16 14:47:02 +1000

    Create server component

--- src/app/Sections/section-health/section-health.component.ts
+++ src/app/Sections/section-health/section-health.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Server } from 'src/app/shared/server';
+
+const SAMPLE_SERVERS = [
+  { id: 1, name: 'dev-web', isOnline: true },
+  { id: 2, name: 'dev-mail', isOnline: false },
+  { id: 3, name: 'prod-web', isOnline: false },
+  { id: 4, name: 'prod-mail', isOnline: true },
+];
+
 @Component({
   selector: 'app-section-health',
   templateUrl: './section-health.component.html',
@@ -8,6 +17,8 @@ import { Component, OnInit } from '@angular/core';
 export class SectionHealthComponent implements OnInit {
 
   constructor() { }
+
+  servers: Server[] = SAMPLE_SERVERS;
 
   ngOnInit(): void {
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { BarChartComponent } from './charts/bar-chart/bar-chart.component';
 import { PieChartComponent } from './charts/pie-chart/pie-chart.component';
 import { LineChartComponent } from './charts/line-chart/line-chart.component';
 import {NgChartsModule} from "ng2-charts";
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import {NgChartsModule} from "ng2-charts";
     SectionHealthComponent,
     BarChartComponent,
     PieChartComponent,
-    LineChartComponent
+    LineChartComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
